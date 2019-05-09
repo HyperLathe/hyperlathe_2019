3b5efae35b7181764d6d8955c83a7ea35c335c81
Richard Young <info@hyperlathe.com>
2019-05-09 18:58:17 +0100

    Further content and json setup

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,32 @@
 
 import React from "react";
+import Content from "./content/content.json";
+import ProfilePic from "./img/richardyoung.jpg"
+
+const pageData = (Content[0].page[0]);
 
 function Home() {
-    return (
-      <div className="Home">
-        <h2>Richard Young</h2>
-        <h3>UX/UI designer / front-end designer & developer</h3>
-        <p>Hello! Welcome to my online portfolio, where you can see some examples of my design and development work.</p>
-        <p>I have been creating web-related content since 2004, including wireframing, UX, prototyping, design, development, and testing.</p>
-        <p>I am a huge fan of responsive design, accessibility, and clean, semantic markup, and have enjoyed working in a number of industries including e-commerce, finance, communications, and insurance.</p>
-        <p>Prior to my current career path, I worked as a composer and copywriter; both of which I found to be useful methodologies in regard to what I'm doing today.</p>
-        <p>Feel free to contact me or call on +44 (0)7967 639 192 if you would like to discuss working together..</p>
+  return (
+    <div className="Home">
+      <div className="top-section">
+        <h2>{pageData.title}</h2>
       </div>
-    );
-  }
+      <div className="row">
+        <div className="col-xs-12 col-sm-6 col-md-6">
+          <h3>{pageData.headline}</h3>
+          <p>{pageData.bodycopy[0]}</p>
+          <p>{pageData.bodycopy[1]}</p>
+          <p>{pageData.bodycopy[2]}</p>
+          <p>{pageData.bodycopy[3]}</p>
+          <p>{pageData.bodycopy[4]}</p>
+        </div>
+        <div className="col-xs-12 col-sm-6 col-md-6">
+          <img src={ProfilePic} className="img-fluid" alt="Richard" />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 
 export default Home;
